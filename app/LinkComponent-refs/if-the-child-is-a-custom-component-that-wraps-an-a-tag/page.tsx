b7b0aa1027502805d2e44eee57f41f7f82141541
ReggieAlbiosA <pureprogrammingph@gmail.com>
2025-02-26 19:01:15 +0800

    fixed build time error instance of 3

--- app/LinkComponent-refs/if-the-child-is-a-custom-component-that-wraps-an-a-tag/page.tsx
+++ app/LinkComponent-refs/if-the-child-is-a-custom-component-that-wraps-an-a-tag/page.tsx
@@ -33,28 +33,60 @@ export default function NavLink() {
     ];
 
     const code = `
-    <ul className='list-disc pl-[20px]'>
-      {linkComponents.map(({ component, color }, index) => (
-        <li key={index}>
-          <Link 
-            href={\`/LinkComponent-refs/if-the-child-is-a-custom-component-that-wraps-an-a-tag#\${color}\`}
-            replace 
-            passHref 
-            legacyBehavior
-            scroll={false}
-          >
-            {component}
-          </Link>
-        </li>
-      ))}
-    </ul>
-    `;
+
+    export function RedLink() {
+    return <a className="text-[red] underline">red link</a>;
+    }
+    export function BlueLink() {
+        return <a className="text-[blue] underline">blue link</a>;
+    }
+    export function GreenLink() {
+        return <a className="text-[green] underline">green link</a>;
+    }
+    export function YellowLink() {
+        return <a className="text-[yellow] underline">yellow link</a>;
+    }
+    export function PurpleLink() {
+        return <a className="text-[purple] underline">purple link</a>;
+    }
+    export function OrangeLink() {
+        return <a className="text-[orange] underline">orange link</a>;
+    }
+
+    export default function NavLink() {
+        const linkComponents = [
+            { component: <BlueLink />, color: 'blue' },
+            { component: <GreenLink />, color: 'green' },
+            { component: <YellowLink />, color: 'yellow' },
+            { component: <PurpleLink />, color: 'purple' },
+            { component: <OrangeLink />, color: 'orange' }
+        ];
+
+        return (
+            <ul className='list-disc pl-[20px]'>
+            {linkComponents.map(({ component, color }, index) => (
+                <li key={index}>
+                <Link 
+                    href={\`/LinkComponent-refs/if-the-child-is-a-custom-component-that-wraps-an-a-tag#\${color}\`}
+                    replace 
+                    passHref 
+                    legacyBehavior
+                    scroll={false}
+                >
+                    {component}
+                </Link>
+                </li>
+            ))}
+            </ul>
+        );
+        `;
+
 
     return (
         <section className="w-full max-w-[850px] mx-auto pt-[50px] flex flex-col gap-y-[40px]">
             <div>
                 <h1 className="font-bold text-[2rem]">
-                    Custom Component that wraps an {`<a>`} tag from next.js Link Component
+                    Custom Component that wraps an <span>&lt;a&gt;</span> tag from next.js Link Component
                 </h1>
                 <hr className="border-b-[2px] border-[#222126]" />
             </div>
@@ -91,8 +123,8 @@ export default function NavLink() {
 
             <p>
                 <strong>Note:</strong> If the child of Link is a custom component that wraps an
-                {`<a>`} tag, you must add <code>passHref</code> to Link. This is necessary if you’re
-                using libraries like styled-components. Without this, the {`<a>`}; tag will not have
+                <span>&lt;a&gt;</span> tag, you must add <code>passHref</code> to Link. This is necessary if you’re
+                using libraries like styled-components. Without this, the <span>&lt;a&gt;</span> tag will not have
                 the href attribute, which hurts your site's accessibility and might affect SEO.
             </p>
         </section>
