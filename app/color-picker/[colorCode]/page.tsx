4146e35ba31479f2fd2732d4a9bca03812b23d24
ReggieAlbiosA <pureprogrammingph@gmail.com>
2025-03-21 05:59:25 +0800

    removed diplicated dir which result into having parallel routes which conflicts each other and fix configuration errors

--- app/color-picker/[colorCode]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Page({ params }: { params: Promise<{  colorCode: string }> }) {
-    const { colorCode } = await params;
-
-    return (
-        <h1>Color Code: #{colorCode}</h1>
-    );
-}
--- app/linking-and-navigating/Link/checking-active-links/page.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function Page() {
-    return (
-        <h1>This is Home page</h1>
-    );
-}
